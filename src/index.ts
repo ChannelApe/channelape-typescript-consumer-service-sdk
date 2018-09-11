43a02f3057ebf5cecfdbee6e59c31b1fb5ef47f4
Frank Mazzarella <fmazzarella@channelape.com>
2018-09-11 16:22:01 -0400

    Adding common services
    s3 uploader, xml parsing, gzip decompression

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export { default as Secrets } from './service/Secrets';
+import * as Parser from './parser/Parser';
+
+export { Parser };
+export { default as SqsMessageService } from './sqs/service/SqsMessageService';
+export { default as DecompressionService } from './decompression/service/DecompressionService';
+export { default as S3Service } from './s3/service/S3Service';
---
+++ src/s3/service/S3Service.ts
@@ -0,0 +1,37 @@
+import { S3 } from 'aws-sdk';
+
+export default class S3Uploader {
+  private s3: S3;
+  private awsS3Bucket: string;
+
+  constructor(awsAccessKeyId: string, awsSecretAccessKey: string, awsRegion: string, awsS3Bucket: string) {
+    this.awsS3Bucket = awsS3Bucket;
+    this.s3 = new S3({
+      accessKeyId: awsAccessKeyId,
+      secretAccessKey: awsSecretAccessKey,
+      region: awsRegion
+    });
+  }
+
+  public uploadFile(fileContents: string, filePrefix: string): Promise<S3.ManagedUpload.SendData> {
+    return new Promise((resolve, reject) => {
+      const fileName = this.getS3Key(filePrefix);
+      this.s3.upload({
+        Body: fileContents,
+        Key: fileName,
+        Bucket: this.awsS3Bucket
+      }, (err: Error, data: S3.ManagedUpload.SendData) => {
+        if (err) {
+          reject(new Error(`Uploading to S3 FAILED with: "${err.message}"`));
+          return;
+        }
+        resolve(data);
+      });
+    });
+  }
+
+  private getS3Key(filePrefix: string): string {
+    const date = new Date().toISOString();
+    return `${filePrefix}_${date}.csv`;
+  }
+}
